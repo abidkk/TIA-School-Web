aec5ec4624eda6da3bf587e4a6297218c824bef0
abidkk <dev.abidkk@gmail.com>
2024-02-06 22:57:30 +0500

    updated

--- src/components/custom_compoents/services_components/OurFeatures.tsx
+++ src/components/custom_compoents/services_components/OurFeatures.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import design from "@/../public/design.jpg"
+import clab from "@/../public/design.jpg"
+import library from "@/../public/library2.jpg"
+import classroom from "@/../public/classroo.jpg"
+import light from "@/../public/light.jpg"
+import scholarship from "@/../public/scholarship.jpg"
 const OurFeatures = () => {
     return (
         <>
@@ -12,30 +16,30 @@ const OurFeatures = () => {
 
                     {/* F-1 */}
                     <div className='p-1 bg-gray-200'>
-                        <Image src={design} width={1000} height={1000} alt='design' className='w-64 h-46' />
+                        <Image src={clab} width={1000} height={1000} alt='design' className='w-64 h-46' />
                         <h3 className='text-center py-1 font-bold text-xl text-green-800'>Computer Lab</h3>
                     </div>
 
                     {/* F-2 */}
                     <div className='p-1 bg-gray-200'>
-                        <Image src={design} width={1000} height={1000} alt='design' className='w-64 h-46' />
+                        <Image src={library} width={1000} height={1000} alt='design' className='w-64 h-46' />
                         <h3 className='text-center py-1 font-bold text-xl text-green-800'>Library</h3>
                     </div>
                     {/* F-3 */}
                     <div className='p-1 bg-gray-200'>
-                        <Image src={design} width={1000} height={1000} alt='design' className='w-64 h-46' />
+                        <Image src={classroom} width={1000} height={1000} alt='design' className='w-64 h-46' />
                         <h3 className='text-center py-1 font-bold text-xl text-green-800'>Safe Environment</h3>
                     </div>
 
                     {/* F-4 */}
                     <div className='p-1 bg-gray-200'>
-                        <Image src={design} width={1000} height={1000} alt='design' className='w-64 h-46' />
+                        <Image src={light} width={1000} height={1000} alt='design' className='w-64 h-46' />
                         <h3 className='text-center py-1 font-bold text-xl text-green-800'>24/7 Electricity </h3>
                     </div>
 
                     {/* F-5 */}
                     <div className='p-1 bg-gray-200'>
-                        <Image src={design} width={1000} height={1000} alt='design' className='w-64 h-46' />
+                        <Image src={scholarship} width={1000} height={1000} alt='design' className='w-64 h-46' />
                         <h3 className='text-center py-1 font-bold text-xl text-green-800'>Scholarships</h3>
                     </div>
                 </div>
