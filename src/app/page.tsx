ff84c6b37035aa1ff2c5bcd1022e0e558328762a
abidkk <dev.abidkk@gmail.com>
2024-02-06 08:21:53 +0500

    faculty

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Testimonials from '@/components/custom_compoents/Testimonials'
 import Hero from '@/components/custom_compoents/hero_components/Hero'
 import OurFeatures from '@/components/custom_compoents/services_components/OurFeatures'
 import OurServices from '@/components/custom_compoents/services_components/OurServices'
-import React from 'react'
+import OurFaculty from '@/components/custom_compoents/services_components/OurFaculty'
+import Gallary from '@/components/custom_compoents/services_components/Gallary'
 
 const page = () => {
   return (
@@ -11,9 +12,9 @@ const page = () => {
   <Hero/>
   <OurServices/>
   <OurFeatures/>
-  <div className='bg-gray-100 py-10'>
-
-  </div>
+  <OurFaculty/>
+  <Gallary/>
+  <div className='h-20 bg-white'></div>
   <Testimonials/>
     </>
   )
---
+++ src/components/custom_compoents/services_components/OurFaculty.tsx
@@ -0,0 +1,146 @@
+import React from 'react'
+import Image from 'next/image'
+import faculty from "@/../public/userlogo.png"
+
+const OurFaculty = () => {
+  return (
+    <>
+           <section className="text-gray-600 body-font bg-gray-100">
+                    <div className="container px-5 py-16 mx-auto">
+                        <div className="flex flex-col text-center w-full mb-10">
+                            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Our Team</h1>
+                            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
+                        </div>
+
+                        <div className="flex flex-wrap -m-2 ">
+
+                            {/* 01 */}
+                            <div className="p-2 lg:w-1/3 md:w-1/2 w-full ">
+                                <div className="h-full flex items-center bg-gray-100 border-gray-300 border p-4 rounded-lg shadow-sm cursor-pointer hover:bg-gray-200 duration-500 ">
+                                    <Image alt="team" className="w-16 h-16 md:w-20 md:h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={faculty} />
+                                    <div className="flex-grow">
+                                        <h2 className="text-gray-900 title-font font-medium">Holden Caulfield</h2>
+                                        <p className="text-gray-500">UI Designer</p>
+                                        <p className='text-gray-400'>0312-2646268</p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            {/* 02 */}
+                            <div className="p-2 lg:w-1/3 md:w-1/2 w-full ">
+                                <div className="h-full flex items-center bg-gray-100 border-gray-300 border p-4 rounded-lg shadow-sm cursor-pointer hover:bg-gray-200 duration-500 ">
+                                    <Image alt="team" className="w-16 h-16 md:w-20 md:h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={faculty} />
+                                    <div className="flex-grow">
+                                        <h2 className="text-gray-900 title-font font-medium">Holden Caulfield</h2>
+                                        <p className="text-gray-500">UI Designer</p>
+                                        <p className='text-gray-400'>0312-2646268</p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+                            {/* 03 */}
+                            <div className="p-2 lg:w-1/3 md:w-1/2 w-full ">
+                                <div className="h-full flex items-center bg-gray-100 border-gray-300 border p-4 rounded-lg shadow-sm cursor-pointer hover:bg-gray-200 duration-500 ">
+                                    <Image alt="team" className="w-16 h-16 md:w-20 md:h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={faculty} />
+                                    <div className="flex-grow">
+                                        <h2 className="text-gray-900 title-font font-medium">Holden Caulfield</h2>
+                                        <p className="text-gray-500">UI Designer</p>
+                                        <p className='text-gray-400'>0312-2646268</p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            {/* 04 */}
+                            <div className="p-2 lg:w-1/3 md:w-1/2 w-full ">
+                                <div className="h-full flex items-center bg-gray-100 border-gray-300 border p-4 rounded-lg shadow-sm cursor-pointer hover:bg-gray-200 duration-500 ">
+                                    <Image alt="team" className="w-16 h-16 md:w-20 md:h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={faculty} />
+                                    <div className="flex-grow">
+                                        <h2 className="text-gray-900 title-font font-medium">Holden Caulfield</h2>
+                                        <p className="text-gray-500">UI Designer</p>
+                                        <p className='text-gray-400'>0312-2646268</p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            {/* 05 */}
+                            <div className="p-2 lg:w-1/3 md:w-1/2 w-full ">
+                                <div className="h-full flex items-center bg-gray-100 border-gray-300 border p-4 rounded-lg shadow-sm cursor-pointer hover:bg-gray-200 duration-500 ">
+                                    <Image alt="team" className="w-16 h-16 md:w-20 md:h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={faculty} />
+                                    <div className="flex-grow">
+                                        <h2 className="text-gray-900 title-font font-medium">Holden Caulfield</h2>
+                                        <p className="text-gray-500">UI Designer</p>
+                                        <p className='text-gray-400'>0312-2646268</p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            {/* 06 */}
+                            <div className="p-2 lg:w-1/3 md:w-1/2 w-full ">
+                                <div className="h-full flex items-center bg-gray-100 border-gray-300 border p-4 rounded-lg shadow-sm cursor-pointer hover:bg-gray-200 duration-500 ">
+                                    <Image alt="team" className="w-16 h-16 md:w-20 md:h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={faculty} />
+                                    <div className="flex-grow">
+                                        <h2 className="text-gray-900 title-font font-medium">Holden Caulfield</h2>
+                                        <p className="text-gray-500">UI Designer</p>
+                                        <p className='text-gray-400'>0312-2646268</p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            {/* 07 */}
+                            <div className="p-2 lg:w-1/3 md:w-1/2 w-full ">
+                                <div className="h-full flex items-center bg-gray-100 border-gray-300 border p-4 rounded-lg shadow-sm cursor-pointer hover:bg-gray-200 duration-500 ">
+                                    <Image alt="team" className="w-16 h-16 md:w-20 md:h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={faculty} />
+                                    <div className="flex-grow">
+                                        <h2 className="text-gray-900 title-font font-medium">Holden Caulfield</h2>
+                                        <p className="text-gray-500">UI Designer</p>
+                                        <p className='text-gray-400'>0312-2646268</p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+                            {/* 08 */}
+                            <div className="p-2 lg:w-1/3 md:w-1/2 w-full ">
+                                <div className="h-full flex items-center bg-gray-100 border-gray-300 border p-4 rounded-lg shadow-sm cursor-pointer hover:bg-gray-200 duration-500 ">
+                                    <Image alt="team" className="w-16 h-16 md:w-20 md:h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={faculty} />
+                                    <div className="flex-grow">
+                                        <h2 className="text-gray-900 title-font font-medium">Holden Caulfield</h2>
+                                        <p className="text-gray-500">UI Designer</p>
+                                        <p className='text-gray-400'>0312-2646268</p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            {/* 09 */}
+                            <div className="p-2 lg:w-1/3 md:w-1/2 w-full ">
+                                <div className="h-full flex items-center bg-gray-100 border-gray-300 border p-4 rounded-lg shadow-sm cursor-pointer hover:bg-gray-200 duration-500 ">
+                                    <Image alt="team" className="w-16 h-16 md:w-20 md:h-20 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={faculty} />
+                                    <div className="flex-grow">
+                                        <h2 className="text-gray-900 title-font font-medium">Holden Caulfield</h2>
+                                        <p className="text-gray-500">UI Designer</p>
+                                        <p className='text-gray-400'>0312-2646268</p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+
+
+                        </div>
+                    </div>
+                </section>
+    </>
+  )
+}
+
+export default OurFaculty
